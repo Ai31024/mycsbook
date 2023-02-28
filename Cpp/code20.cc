5e752245a325990e1b6fb1fa8a9129e7e90f36bc
Ai31024 <iazam44@hotmail.com>
2023-02-28 23:29:11 +0530

    update

--- Cpp/code20.cc
+++ Cpp/code20.cc
@@ -1,1 +1,29 @@
 // Array of object & passing objects as function argument.
+#include<iostream>
+class employee
+{
+	int id;
+	int salary;
+public:
+	void setData(void)
+	{
+		salary = 10000;
+		std::cout << "Enter the ID of Employee : ";
+		std::cin >> id;
+	}
+	void getData(void)
+	{
+		std::cout << "The Employee ID is : " << id << "\n";
+	}
+};
+
+int main()
+{
+	employee abc[10];
+	for (int i = 0; i < 10; i++)
+	{
+		abc[i].setData();
+		abc[i].getData();
+	}
+	return 0;
+}
---
+++ Cpp/code20_1.cc
@@ -0,0 +1,38 @@
+// Array of object & passing objects as function argument.
+#include<iostream>
+class complex
+{
+    int a;
+    int b;
+public:
+    void setData(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+    void setDatabySum(complex o1, complex o2)
+    {
+        a = o1.a + o2.a;
+        b = o1.b + o2.b;
+    }
+    void printvalues(void)
+    {
+        std::cout<<"Your complex number is "<<a<<"+"<<b<<"i"<<"\n";
+    }
+};
+
+int main()
+{
+    complex c1, c2, c3;
+
+    c1.setData(1,4);
+    c1.printvalues();
+
+    c2.setData(3,7);
+    c2.printvalues();
+
+    c3.setDatabySum(c1,c2);
+    c3.printvalues();
+
+    return 0;
+}
