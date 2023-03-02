3650b7cca1cade6ccf18c5f2186495bbb95ef2c7
Ai31024 <iazam44@hotmail.com>
2023-03-02 22:39:07 +0530

    practice

---
+++ mytakes/mytake12.cc
@@ -0,0 +1,12 @@
+//Swapping number without using third variable or using pointers and reference
+#include<iostream>
+int main()
+{
+    int a, b;
+    std::cout<<"Enter value of a : ";
+    std::cin>>a;
+    std::cout<<"Enter value of b : ";
+    std::cin>>b;
+    std::cout<<"a: "<<a-a+b<<" b: "<<b-b+a<<"\n";
+    return 0;
+}
