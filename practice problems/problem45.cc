e61c35a55f8e009c14b261d8961b1334a173fb44
Ai31024 <iazam44@hotmail.com>
2023-03-12 23:03:03 +0530

    practice

---
+++ practice problems/problem45.cc
@@ -0,0 +1,11 @@
+/*Write a program to print all prime numbers from 1 to 300.
+(Hint: Use nested loops, break and continue)*/
+#include <iostream>
+int main()
+{
+    for (int i = 1; i < 300; i++)
+    {
+        //
+    }
+    return 0;
+}
