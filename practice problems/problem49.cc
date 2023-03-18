737d8ac5f13ae0c12b5cf5b62b7148079dd40a5d
Ai31024 <iazam44@hotmail.com>
2023-03-18 19:47:52 +0530

    practice

--- practice problems/problem49.cc
+++ practice problems/problem49.cc
@@ -2,44 +2,27 @@
 with side length less than or equal to 30.*/
 
 #include <iostream>
-
-void pythagoreanTriplets(int limit)
+int main()
 {
-
-    // triplet: a^2 + b^2 = c^2
     int a, b, c = 0;
-
-    // loop from 2 to max_limit
     int m = 2;
-
-    // Limiting c would limit
-    // all a, b and c
+    int limit = 30;
     while (c < limit)
     {
-
-        // now loop on j from 1 to i-1
         for (int n = 1; n < m; ++n)
         {
-
-            // Evaluate and print triplets using
-            // the relation between a, b and c
             a = m * m - n * n;
             b = 2 * m * n;
             c = m * m + n * n;
 
             if (c > limit)
+            {
                 break;
+            }
 
-            printf("%d %d %d\n", a, b, c);
+            std::cout << a << " " << b << " " << c << "\n";
         }
         m++;
     }
-}
-
-// Driver Code
-int main()
-{
-    int limit = 30;
-    pythagoreanTriplets(limit);
     return 0;
 }
