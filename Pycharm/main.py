8207e057c72a74d2b5a8430f22b48ab0101943df
Azam <iazam44@hotmail.com>
2023-07-13 01:41:32 +0530

    python practice

--- Pycharm/main.py
+++ Pycharm/main.py
@@ -1,12 +1,42 @@
-print('Welcome to Bill payment')  # 'Welcome line'
+print("\nWelcome to the Love Calculator!\n")
 
-totalBill = float(input('Please enter the total bill: '))
-splitToPeople = float(input('How many people are there to split the bill?: '))
-addTipAmount = float(input('How much tip in percent to bill will you add?: '))
+yourName1 = input("What is your name? \n")
+theirName2 = input("What is their name? \n")
 
-tipPercent = addTipAmount*0.01
-grandTotal = totalBill + (totalBill*tipPercent)
-splitAmount = grandTotal / splitToPeople
+yourName = yourName1.lower()
+theirName = theirName2.lower()
 
-print(f'Your Grand Total is : {grandTotal}')
-print(f'and each person pays: {splitAmount}')
+your_T = int(yourName.count("t"))
+your_R = int(yourName.count("r"))
+your_U = int(yourName.count("u"))
+your_E = int(yourName.count("e"))
+
+your_L = int(yourName.count("l"))
+your_O = int(yourName.count("o"))
+your_V = int(yourName.count("v"))
+your_E = int(yourName.count("e"))
+
+their_T = int(theirName.count("t"))
+their_R = int(theirName.count("r"))
+their_U = int(theirName.count("u"))
+their_E = int(theirName.count("e"))
+
+their_L = int(theirName.count("l"))
+their_O = int(theirName.count("o"))
+their_V = int(theirName.count("v"))
+their_E = int(theirName.count("e"))
+
+firstDigit = str(your_T + your_R + your_U + your_E + their_T + their_R + their_U + their_E)
+
+secondDigit = str(your_L + your_O + your_V + your_E + their_L + their_O + their_V + their_E)
+
+core = firstDigit + secondDigit
+
+loveScore = int(core)
+
+if loveScore < 10 or loveScore > 90:
+    print(f"Your love score is {loveScore}, you go together like soda and mint.")
+elif loveScore > 40 and loveScore < 50:
+    print(f"Your love score is {loveScore}, you are alright together.")
+else:
+    print(f"Your love score is {loveScore}.")
